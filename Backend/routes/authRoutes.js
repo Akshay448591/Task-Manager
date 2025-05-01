d07f868082aed4e42d8629dd91a5f93a318356dc
Akshay448591 <akshayyadav12356@gmail.com>
2025-05-01 21:36:31 +0530

    Authentication Api is all set and image upload api is done

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -5,12 +5,23 @@ const {
   getUserProfile,
   updateUserProfile,
 } = require("../controllers/authController");
-const {protect}= require("../middlewares/authMiddleware")
+const { protect } = require("../middlewares/authMiddleware");
+const upload = require("../middlewares/uploadMiddleware");
 const router = express.Router();
 //Auth Routes
 router.post("/register", registerUser); // Register User
 router.post("/login", loginUser); // Login User
 router.get("/profile", protect, getUserProfile); //  Get User Profile
 router.put("/profile", protect, updateUserProfile); //  Update User
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file Upload" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
 
 module.exports = router;
